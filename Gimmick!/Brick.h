0fc8d222165157ce791e92a41853c14f72357b68
SIMP lỏd <67738980+19522541@users.noreply.github.com>
2021-05-18 10:25:34 +0700

    camera_

--- Gimmick!/Brick.h
+++ Gimmick!/Brick.h
@@ -1,27 +1,27 @@
 #pragma once
 
-#include "./../../GameComponents/Animation.h"
-#include "../Entity.h"
+#include "GameComponents/Animation.h"
+#include "GameObjects/Entity.h"
 #include "GameObject.h"
 
 //vien gach
-class DynamicObject : public Entity
+class Brick : public Entity
 {
 public:
-    ~DynamicObject();
+    ~Brick();
 
-    void Update(float dt);
+   /* void Update(float dt);
 
     void Draw(D3DXVECTOR3 position = D3DXVECTOR3(), RECT sourceRect = RECT(), D3DXVECTOR2 scale = D3DXVECTOR2(), D3DXVECTOR2 transform = D3DXVECTOR2(), float angle = 0, D3DXVECTOR2 rotationCenter = D3DXVECTOR2(), D3DXCOLOR colorKey = D3DCOLOR_XRGB(255, 255, 255));
 
-    void Draw(D3DXVECTOR2 transform);
+    void Draw(D3DXVECTOR2 transform);*/
 
 protected:
-    DynamicObject();
+    Brick();
 
     bool init(D3DXVECTOR3 position);
 
-    void OnSetPosition(D3DXVECTOR3 poks);
+  //  void OnSetPosition(D3DXVECTOR3 poks);
 
     virtual const char* FileName() = 0; // bat buoc phai override ham nay
     virtual int TotalFrame() = 0; //so frame cua animation
@@ -29,5 +29,5 @@ protected:
     virtual int Column() = 0; // so cot cua resource hinh anh animation
     virtual float SecondPerFrame() = 0; //so giay de chuyen frame cua animation
 
-    Animation *mAnimation;
+    Animation* mAnimation;
 };
